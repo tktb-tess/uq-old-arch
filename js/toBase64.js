674efe3c287d3effd6514e1be0abb6412c2fde19
Born-Bohrer <chem1000leaves@gmail.com>
2024-12-14 21:55:46 +0900

    Base64 Encoderなどを追加

---
+++ js/toBase64.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const base64_btn = document.getElementById("base64-btn");
+const base64de_btn = document.getElementById("base64de-btn");
+
+base64_btn.addEventListener("click", () => {
+    const base64_input = document.getElementById("base64-input");
+    const base64_result = document.getElementById("base64-result");
+    base64_result.value =  Base64Encoder(base64_input.value);
+}, false);
+
+base64de_btn.addEventListener("click", () => {
+    const base64de_input = document.getElementById("base64de-input");
+    const base64de_result = document.getElementById("base64de-result");
+    base64de_result.value = Base64Decoder(base64de_input.value);
+}, false);
+
+
+
+const Base64Encoder = (str) => {
+    const utf8Arr = new TextEncoder().encode(str);
+    const binStr = Array.from(utf8Arr, (bin) => String.fromCodePoint(bin)).join("");
+    return btoa(binStr);
+}
+
+const Base64Decoder = (base64) => {
+    const binStr = atob(base64);
+    // console.log("binStr: " + binStr);
+    const utf8arr = Uint8Array.from(binStr, (c) => c.codePointAt(0));
+    // console.log("utf8arr: " + utf8arr);
+    
+    // console.log("str: " + str);
+    return new TextDecoder().decode(utf8arr);
+
+}
+
+
+
+
