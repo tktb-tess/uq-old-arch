ef6e0e29ead81b2aa1ff9de950427fe91397f553
Born-Bohrer <chem1000leaves@gmail.com>
2024-12-22 19:45:15 +0900

    一旦

--- js/tools.js
+++ js/tools.js
@@ -52,10 +52,53 @@ base64de_btn.addEventListener("click", () => {
     }
 }, false);
 
+class RSA {
+    #p; #q; #pq; #phi_pq;
+    
+    constructor(val_p, val_q) {
+        if (typeof(val_p) !== 'number' || typeof(val_q) !== 'number') throw new TypeError("Die Argumentstypen sind keine `number`.");
+        this.#p = val_p;
+        this.#q = val_q;
+        this.#pq = this.#p * this.#q;
+        this.#phi_pq = (this.#p - 1) * (this.#q - 1);
+    }
 
+    static primzahlIst(n) {
+        if (typeof(n) !== 'number') throw new TypeError("Der Argumentstyp ist kein `number`.");
+        if (n < 2) return false;
+        const limit = Math.floor(Math.sqrt(n));
 
+        return true;
 
+    }
 
+    // max以下の素数の配列を返す
+    static primListeMachen(max) { 
+        if (typeof(max) !== 'number') {
+            throw new TypeError("Der Argumentstyp ist kein `number`.");
+        }
+        if (max > 500000) {
+            throw new Error("値が大きすぎます。\n500,000以下の値を入力してください。");
+        }
+        let prim_liste = [...Array(max - 1)].map((_, i) => i + 2);
+        
+        for (let i = 0; i < prim_liste.length; i++) {
+            const p_ = prim_liste[i];
+            for (let j = i + 1; j < prim_liste.length; j++) {
+                if (prim_liste[j] % p_ === 0) {
+                    prim_liste.splice(j, 1);
+                }
+                
+            }
+        }
+        return prim_liste;
+    }
+}
 
-
+new Promise((resolve) => {
+    setTimeout(() => {
+        const result = RSA.primListeMachen(200000);
+        resolve(result);
+    }, 4000);
+}).then((val) => console.log(val));
 
