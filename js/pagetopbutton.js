de3ef01230875c558e2d47a83b30f0857c229537
Born-Bohrer <chem1000leaves@gmail.com>
2025-02-10 13:16:09 +0900

    update: Miller-Rabin 追加

--- js/pagetopbutton.js
+++ js/pagetopbutton.js
@@ -1,16 +1,20 @@
-const pageTopBtn = document.getElementById('page-top-button');
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 500) {
-        pageTopBtn.dataset.visible = true;
-    } else {
-        delete pageTopBtn.dataset.visible;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    const pageTopBtn = document.getElementById('page-top-button');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+            pageTopBtn.dataset.visible = true;
+        } else {
+            delete pageTopBtn.dataset.visible;
+        }
+        
+    }, false);
     
+    pageTopBtn.addEventListener('click', function() {
+        window.scroll({ top: 0 });
+    }, false);
 }, false);
 
-pageTopBtn.addEventListener('click', function() {
-    window.scroll({ top: 0 });
-}, false);
+
+
 
  
